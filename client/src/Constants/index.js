9f7305c6a447e1a1b522293b3c3837c8d06f8484
Nathan Innocent Amy <89917308+OnePunchKo@users.noreply.github.com>
2022-04-06 23:06:32 -0400

    Added Color

--- client/src/Constants/index.js
+++ client/src/Constants/index.js
@@ -1,16 +1,78 @@
-export const COLORS = {
- // Bright purple:
- primary: "hsl(258deg, 100%, 50%)",
- primaryHover: "hsl(258deg, 100%, 90%)",
+export const COLOR = {
+ //Primary = card color
+ //Secondary = text color
 
- buttonPrimary: "hsl(265deg, 98%, 67%)",
- buttonHover: "hsl(258deg, 100%, 35%)",
+ // Normal type: | Gray
+ normal_primary: "hsl(240deg, 14%, 89%)",
+ normal_secondary: "hsl(217deg, 8%, 57%)",
 
- // Grey color
- secondary: "hsl(204deg, 8%, 26%)",
- secondaryHover: "hsl(204deg, 8%, 86%)",
+ // Fire type: | Orange/red
+ fire_primary: "hsl(265deg, 98%, 67%)",
+ fire_secondary: "hsl(258deg, 100%, 35%)",
 
- // Add more colors as needed!
+ //Water type: | Blue
+ water_primary: "hsl(210deg, 68%, 82%)",
+ water_secondary: "hsl(210deg, 44%, 39%)",
+
+ // Grass type | Green
+ grass_primary: "hsl(161deg, 92%, 86%)",
+ grass_secondary: "hsl(147deg, 83%, 34%)",
+
+ // Bug type: | Green ish
+ bug_primary: "hsl(75%, 48%, 84%)",
+ bug_secondary: "hsl(76%, 26%, 41%)",
+
+ // Electric type: | Yellow
+ electric_primary: "hsl(60%, 17%, 88%)",
+ electric_secondary: "hsl(44%, 61%, 57%)",
+
+ // Fairy type: | Pink
+ fairy_primary: "hsl(266%, 40%, 90%)",
+ fairy_secondary: "hsl(292%, 25%, 60%)",
+
+ // Psychic type: | red
+ psychic_primary: "hsl(329%, 30%, 86%)",
+ psychic_secondary: "hsl(346%, 35%, 56%)",
+
+ // Fighting type: |
+ fighting_primary: "hsl(339deg, 75%, 80%)",
+ fighting_secondary: "hsl(339deg, 75%, 53%)",
+
+ // Rock type: |
+ rock_primary: "hsl(44deg, 38%, 86%)",
+ rock_secondary: "hsl(44deg, 38%, 66%)",
+
+ // Ground type: |
+ ground_primary: "hsl(21deg, 81%, 86%)",
+ ground_secondary: "hsl(21deg, 81%, 56%)",
+
+ // Flying type: |
+ flying_primary: "hsl(218%, 63%, 92%)",
+ flying_secondary: "hsl(218%, 63%, 72%)",
+
+ // Ghost type: |
+ ghost_primary: "hsl(222%, 41%, 70%)",
+ ghost_secondary: "hsl(222%, 41%, 50%)",
+
+ // Ice type: |
+ ice_primary: "hsl(172%, 60%, 76%)",
+ ice_secondary: "hsl(172%, 60%, 56%)",
+
+ // Poison type: |
+ poison_primary: "hsl(286%, 52%, 81%)",
+ poison_secondary: "hsl(286%, 52%, 61%)",
+
+ // Steel type: |
+ steel_primary: "hsl(196%, 29%, 79%)",
+ steel_secondary: "hsl(196%, 29%, 49%)",
+
+ // Dragon type |
+ dragon_primary: "hsl(207%, 100%, 60%)",
+ dragon_secondary: "hsl(207%, 100%, 40%)",
+
+ //Dark type: |
+ dark_primary: "hsl(265, 10%, 56%)",
+ dark_secondary: "hsl(265%, 10%, 36%)",
 };
 
 export const SIZE = {
