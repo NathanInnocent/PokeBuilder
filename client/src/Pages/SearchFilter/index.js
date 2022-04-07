d595769ace62d8d285979f9e53d901b5bba57d6a
Nathan Innocent Amy <89917308+OnePunchKo@users.noreply.github.com>
2022-04-07 11:37:54 -0400

    Layout of filter button

--- client/src/Pages/SearchFilter/index.js
+++ client/src/Pages/SearchFilter/index.js
@@ -1,3 +1,4 @@
+import styled from "styled-components";
 import { TYPE_ICON } from "../../Helpers/Icons";
 
 export const SearchFilterPage = () => {
@@ -6,9 +7,9 @@ export const SearchFilterPage = () => {
    {/* First Container */}
    <div>
     <h1>Search by type</h1>
-    <button>
+    <button style={{ display: "flex", alignItems: "center", justifyContent: "space-between", width: "200px", borderRadius: "30px", border: "none" }}>
      <p>Normal</p>
-     <img src={TYPE_ICON.normal} />
+     <img src={TYPE_ICON.normal} style={{ width: "50px" }} />
     </button>
    </div>
   </>
