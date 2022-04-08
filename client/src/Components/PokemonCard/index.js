ff3bf56d9fb7cc9d4fe142a5f96155ab66c39ce4
Nathan Innocent Amy <89917308+OnePunchKo@users.noreply.github.com>
2022-04-08 16:43:09 -0400

    Fixed Key naming

--- client/src/Components/PokemonCard/index.js
+++ client/src/Components/PokemonCard/index.js
@@ -7,15 +7,16 @@ export const PokemonCard = ({ info }) => {
  const { sprites, id, types, name } = info;
  const image = sprites.other[`official-artwork`][`front_default`];
  const primaryType = types[0].type.name;
- const { background, numberColor, nameColor } = getColorPallate(primaryType);
+ const { backgroundColor, numberColor, nameColor } = getColorPallate(primaryType);
+ const displayedPokemonId = convertPokemonId(id);
 
  const capitalizeFirstLetter = (string) => {
   return string.charAt(0).toUpperCase() + string.slice(1);
  };
 
  return (
-  <SmallPokemonCard style={{ backgroundColor: `${background}` }}>
-   <ID style={{ color: `${numberColor}` }}>{convertPokemonId(id)}</ID>
+  <SmallPokemonCard style={{ backgroundColor: `${backgroundColor}` }}>
+   <ID style={{ color: `${numberColor}` }}>{displayedPokemonId}</ID>
    <Name style={{ color: `${nameColor}` }}>{capitalizeFirstLetter(name)}</Name>
    <div style={{ display: "flex", gap: "10px" }}>
     {types.map((data, index) => {
--- client/src/Components/PokemonCard/typeLogic.js
+++ client/src/Components/PokemonCard/typeLogic.js
@@ -1,67 +1,67 @@
 import { COLOR } from "../../Constants";
 import { TYPE_ICON } from "../../Helpers/Icons";
 
-let colorPalette = { background: "", ID: "", name: "" };
+let colorPalette = { backgroundColor: "", ID: "", name: "" };
 let typeIcon = { icon: "" };
 
 export const getColorPallate = (type) => {
  switch (type) {
   case "normal":
-   colorPalette = { background: COLOR.normal_card, numberColor: COLOR.normal_ID, nameColor: COLOR.normal_name };
+   colorPalette = { backgroundColor: COLOR.normal_card, numberColor: COLOR.normal_ID, nameColor: COLOR.normal_name };
    break;
   case "fire":
-   colorPalette = { background: COLOR.fire_card, numberColor: COLOR.fire_ID, nameColor: COLOR.fire_name };
+   colorPalette = { backgroundColor: COLOR.fire_card, numberColor: COLOR.fire_ID, nameColor: COLOR.fire_name };
    break;
   case "water":
-   colorPalette = { background: COLOR.water_card, numberColor: COLOR.water_ID, nameColor: COLOR.water_name };
+   colorPalette = { backgroundColor: COLOR.water_card, numberColor: COLOR.water_ID, nameColor: COLOR.water_name };
    break;
   case "grass":
-   colorPalette = { background: COLOR.grass_card, numberColor: COLOR.grass_ID, nameColor: COLOR.grass_name };
+   colorPalette = { backgroundColor: COLOR.grass_card, numberColor: COLOR.grass_ID, nameColor: COLOR.grass_name };
    break;
   case "bug":
-   colorPalette = { background: COLOR.bug_card, numberColor: COLOR.bug_ID, nameColor: COLOR.bug_name };
+   colorPalette = { backgroundColor: COLOR.bug_card, numberColor: COLOR.bug_ID, nameColor: COLOR.bug_name };
    break;
   case "electric":
-   colorPalette = { background: COLOR.electric_card, numberColor: COLOR.electric_ID, nameColor: COLOR.electric_name };
+   colorPalette = { backgroundColor: COLOR.electric_card, numberColor: COLOR.electric_ID, nameColor: COLOR.electric_name };
    break;
   case "fairy":
-   colorPalette = { background: COLOR.fairy_card, numberColor: COLOR.fairy_ID, nameColor: COLOR.fairy_name };
+   colorPalette = { backgroundColor: COLOR.fairy_card, numberColor: COLOR.fairy_ID, nameColor: COLOR.fairy_name };
    break;
   case "psychic":
-   colorPalette = { background: COLOR.psychic_card, numberColor: COLOR.psychic_ID, nameColor: COLOR.psychic_name };
+   colorPalette = { backgroundColor: COLOR.psychic_card, numberColor: COLOR.psychic_ID, nameColor: COLOR.psychic_name };
    break;
   case "fighting":
-   colorPalette = { background: COLOR.fighting_card, numberColor: COLOR.fighting_ID, nameColor: COLOR.fighting_name };
+   colorPalette = { backgroundColor: COLOR.fighting_card, numberColor: COLOR.fighting_ID, nameColor: COLOR.fighting_name };
    break;
   case "rock":
-   colorPalette = { background: COLOR.rock_card, numberColor: COLOR.rock_ID, nameColor: COLOR.rock_name };
+   colorPalette = { backgroundColor: COLOR.rock_card, numberColor: COLOR.rock_ID, nameColor: COLOR.rock_name };
    break;
   case "ground":
-   colorPalette = { background: COLOR.ground_card, numberColor: COLOR.ground_ID, nameColor: COLOR.ground_name };
+   colorPalette = { backgroundColor: COLOR.ground_card, numberColor: COLOR.ground_ID, nameColor: COLOR.ground_name };
    break;
   case "flying":
-   colorPalette = { background: COLOR.flying_card, numberColor: COLOR.flying_ID, nameColor: COLOR.flying_name };
+   colorPalette = { backgroundColor: COLOR.flying_card, numberColor: COLOR.flying_ID, nameColor: COLOR.flying_name };
    break;
   case "ghost":
-   colorPalette = { background: COLOR.ghost_card, numberColor: COLOR.ghost_ID, nameColor: COLOR.ghost_name };
+   colorPalette = { backgroundColor: COLOR.ghost_card, numberColor: COLOR.ghost_ID, nameColor: COLOR.ghost_name };
    break;
   case "ice":
-   colorPalette = { background: COLOR.ice_card, numberColor: COLOR.ice_ID, nameColor: COLOR.ice_name };
+   colorPalette = { backgroundColor: COLOR.ice_card, numberColor: COLOR.ice_ID, nameColor: COLOR.ice_name };
    break;
   case "dark":
-   colorPalette = { background: COLOR.dark_card, numberColor: COLOR.dark_ID, nameColor: COLOR.dark_name };
+   colorPalette = { backgroundColor: COLOR.dark_card, numberColor: COLOR.dark_ID, nameColor: COLOR.dark_name };
    break;
   case "dragon":
-   colorPalette = { background: COLOR.dragon_card, numberColor: COLOR.dragon_ID, nameColor: COLOR.dragon_name };
+   colorPalette = { backgroundColor: COLOR.dragon_card, numberColor: COLOR.dragon_ID, nameColor: COLOR.dragon_name };
    break;
   case "poison":
-   colorPalette = { background: COLOR.poison_card, numberColor: COLOR.poison_ID, nameColor: COLOR.poison_name };
+   colorPalette = { backgroundColor: COLOR.poison_card, numberColor: COLOR.poison_ID, nameColor: COLOR.poison_name };
    break;
   case "steel":
-   colorPalette = { background: COLOR.steel_card, numberColor: COLOR.steel_ID, nameColor: COLOR.steel_name };
+   colorPalette = { backgroundColor: COLOR.steel_card, numberColor: COLOR.steel_ID, nameColor: COLOR.steel_name };
    break;
   default:
-   colorPalette = { background: COLOR.normal_card, numberColor: COLOR.normal_ID, nameColor: COLOR.normal_name };
+   colorPalette = { backgroundColor: COLOR.normal_card, numberColor: COLOR.normal_ID, nameColor: COLOR.normal_name };
    break;
  }
  return colorPalette;
