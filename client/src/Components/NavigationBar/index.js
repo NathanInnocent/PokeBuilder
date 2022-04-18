24e99abeb88b034e628552541a82a326e694bb03
Nathan Innocent Amy <89917308+OnePunchKo@users.noreply.github.com>
2022-04-18 16:04:10 -0400

    Fixed hover css

--- client/src/Components/NavigationBar/index.js
+++ client/src/Components/NavigationBar/index.js
@@ -1,103 +1,49 @@
-import { useContext } from "react";
-import { Link, NavLink, useLocation } from "react-router-dom";
-import styled from "styled-components";
+import { useContext, useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
 import { CurrentUserContext } from "../../Context/CurrentUserContext";
-import { HomePng, PokedexPng, SignInPng, SignOutPng, TeamsPng } from "./navigationStyles";
+import { HomeNav, PokedexNav, SignInNav, SignOutNav, TeamNav, NavigationContainer, NavigationDivContainer } from "./navigationStyles";
 
 export const NavigationBar = () => {
  const location = useLocation();
  const { currentUser, setCurrentUser } = useContext(CurrentUserContext);
+ const [locationPath, setLocationPath] = useState(null);
+
+ useEffect(() => {
+  setLocationPath(location.pathname);
+ }, [location.pathname]);
 
  return (
   <>
    {/* Hide navbar on sign in page */}
    {location.pathname !== "/" && (
-    <Container>
-     {/* Home Page */}
-     <LinkContainer to="/home" className={({ isActive }) => (isActive ? "active" : "inactive")}>
-      <HomePng />
+    <NavigationContainer>
+     {/* Home button */}
+     <HomeNav to="/home" active={locationPath === "/home" ? "true" : null}>
       Home
-     </LinkContainer>
-     {/* Pokedex Page */}
-     <LinkContainer to="/pokedex">
-      <PokedexPng />
+     </HomeNav>
+
+     {/* Pokedex button */}
+     <PokedexNav to="/pokedex" active={locationPath === "/pokedex" ? "true" : null}>
       Pokedex
-     </LinkContainer>
-     {/* Teams page */}
-     <LinkContainer to="/teams">
-      <TeamsPng />
-      Teams
-     </LinkContainer>
+     </PokedexNav>
+
+     {/* Team button */}
+     <TeamNav to="/teams" active={locationPath === "/teams" ? "true" : null}>
+      Team
+     </TeamNav>
 
      {/* Sign in */}
-     {!currentUser && (
-      <LinkContainer to="/">
-       <SignInPng />
-       Sign in
-      </LinkContainer>
-     )}
+     {currentUser === null && <SignInNav to="/">Sign In</SignInNav>}
 
      {/* Log Out */}
      {currentUser && (
-      <DivContainer>
-       <SignOutPng onClick={() => setCurrentUser(null)} />
+      <NavigationDivContainer>
+       <SignOutNav onClick={() => setCurrentUser(null)} />
        Sign out
-      </DivContainer>
+      </NavigationDivContainer>
      )}
-    </Container>
+    </NavigationContainer>
    )}
   </>
  );
 };
-
-const Container = styled.div`
- display: flex;
- flex-direction: row;
- justify-content: space-evenly;
- align-items: center;
- color: black;
- font-size: 1.5em;
- font-weight: bold;
- padding: 10px;
- border-radius: 25px;
- box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
- background: gray;
- margin: 30px 30px;
-`;
-
-const LinkContainer = styled(NavLink)`
- display: flex;
- flex-direction: row;
- justify-content: center;
- align-items: center;
- text-decoration: none;
- color: black;
- font-size: 1.5em;
- transition: all 0.2s ease-in-out;
-
- &:hover {
-  background: #8a8a8a;
-  color: red;
- }
-
- &.active {
-  background: #8a8a8a;
-  color: red;
- }
-`;
-
-const DivContainer = styled.div`
- display: flex;
- flex-direction: row;
- justify-content: center;
- align-items: center;
- color: black;
- font-size: 1.5em;
- transition: all 0.2s ease-in-out;
- cursor: pointer;
-
- &:hover {
-  background: #8a8a8a;
-  color: red;
- }
-`;
--- client/src/Components/NavigationBar/navigationStyles.js
+++ client/src/Components/NavigationBar/navigationStyles.js
@@ -1,66 +1,161 @@
+import { NavLink } from "react-router-dom";
 import styled from "styled-components";
 import { NAVBAR } from "../../Helpers/Icons";
 
-export const HomePng = styled.div`
- background-image: url("${NAVBAR.inactiveHome}");
- background-size: 100%;
- background-repeat: no-repeat;
- object-fit: contain;
- width: 80px;
- height: 80px;
+export const HomeNav = styled(NavLink)`
+ display: flex;
+ justify-content: center;
+ align-items: center;
+ display: relative;
+ text-decoration: none;
+ color: ${(props) => (props.active ? "red" : "black")};
+ font-size: 1.5em;
  transition: all 0.2s ease-in-out;
+ pointer-events: all;
 
- &:hover {
-  background-image: url("${NAVBAR.activeHome}");
+ ::before {
+  content: "";
+  display: inline-block;
+  position: relative;
+  top: 0px;
+  left: -5px;
+  width: 80px;
+  height: 80px;
+  background-image: url(${(props) => (props.active ? NAVBAR.activeHome : NAVBAR.inactiveHome)});
+  background-size: 100%;
+  background-repeat: no-repeat;
+  object-fit: contain;
+  z-index: 5;
+  transition: all 0.2s ease-in-out;
+ }
+
+ &:hover,
+ &:focus {
+  outline: none;
+  background: #8a8a8a;
+  color: red;
+
+  ::before {
+   background-image: url("${NAVBAR.activeHome}");
+  }
  }
 `;
 
-export const SignInPng = styled.div`
- background-image: url("${NAVBAR.inactiveSignIn}");
- background-size: 100%;
- background-repeat: no-repeat;
- border: none;
- object-fit: contain;
- width: 80px;
- height: 80px;
+export const SignInNav = styled(NavLink)`
+ display: flex;
+ justify-content: center;
+ align-items: center;
+ display: relative;
+ text-decoration: none;
+ color: ${(props) => (props.active ? "red" : "black")};
+ font-size: 1.5em;
  transition: all 0.2s ease-in-out;
- border: 0;
- border: none;
- outline: none;
+ pointer-events: all;
 
- &:hover {
-  background-image: url("${NAVBAR.activeSignIn}");
+ ::before {
+  content: "";
+  display: inline-block;
+  position: relative;
+  top: 0px;
+  left: -5px;
+  width: 80px;
+  height: 80px;
+  background-image: url(${(props) => (props.active ? NAVBAR.activeSignIn : NAVBAR.inactiveSignIn)});
+  background-size: 100%;
+  background-repeat: no-repeat;
+  object-fit: contain;
+  transition: all 0.2s ease-in-out;
+ }
+
+ &:hover,
+ &:focus {
+  outline: none;
+  background: #8a8a8a;
+  color: red;
+
+  ::before {
+   background-image: url("${NAVBAR.activeSignIn}");
+  }
  }
 `;
 
-export const TeamsPng = styled.div`
- background-image: url("${NAVBAR.inactiveTeam}");
- background-size: 100%;
- background-repeat: no-repeat;
- object-fit: contain;
- width: 80px;
- height: 80px;
+export const TeamNav = styled(NavLink)`
+ display: flex;
+ justify-content: center;
+ align-items: center;
+ display: relative;
+ text-decoration: none;
+ color: ${(props) => (props.active ? "red" : "black")};
+ font-size: 1.5em;
  transition: all 0.2s ease-in-out;
+ pointer-events: all;
 
- &:hover {
-  background-image: url("${NAVBAR.activeTeam}");
+ ::before {
+  content: "";
+  display: inline-block;
+  position: relative;
+  top: 0px;
+  left: -5px;
+  width: 80px;
+  height: 80px;
+  background-image: url(${(props) => (props.active ? NAVBAR.activeTeam : NAVBAR.inactiveTeam)});
+  background-size: 100%;
+  background-repeat: no-repeat;
+  object-fit: contain;
+  transition: all 0.2s ease-in-out;
+ }
+
+ &:hover,
+ &:focus {
+  outline: none;
+  background: #8a8a8a;
+  color: red;
+
+  ::before {
+   background-image: url("${NAVBAR.activeTeam}");
+  }
  }
 `;
-export const PokedexPng = styled.div`
- background-image: url("${NAVBAR.inactivePokedex}");
- background-size: 100%;
- background-repeat: no-repeat;
- object-fit: contain;
- width: 80px;
- height: 80px;
- transition: all 0.2s ease-in-out;
 
- &:hover {
-  background-image: url("${NAVBAR.activePokedex}");
+export const PokedexNav = styled(NavLink)`
+ display: flex;
+ justify-content: center;
+ align-items: center;
+ display: relative;
+ text-decoration: none;
+ color: ${(props) => (props.active ? "red" : "black")};
+ font-size: 1.5em;
+ transition: all 0.2s ease-in-out;
+ pointer-events: all;
+
+ ::before {
+  content: "";
+  display: inline-block;
+  position: relative;
+  top: 0px;
+  left: -5px;
+  width: 80px;
+  height: 80px;
+  background-image: url(${(props) => (props.active ? NAVBAR.activePokedex : NAVBAR.inactivePokedex)});
+  background-size: 100%;
+  background-repeat: no-repeat;
+  object-fit: contain;
+  transition: all 0.2s ease-in-out;
+ }
+
+ &:hover,
+ &:focus {
+  outline: none;
+  background: #8a8a8a;
+  color: red;
+
+  ::before {
+   background-image: url("${NAVBAR.activePokedex}");
+  }
  }
 `;
 
-export const SignOutPng = styled.div`
+export const SignOutNav = styled.div`
  background-image: url("${NAVBAR.inactiveLogout}");
  background-size: 100%;
  background-repeat: no-repeat;
@@ -71,5 +166,37 @@ export const SignOutPng = styled.div`
 
  &:hover {
   background-image: url("${NAVBAR.activeLogout}");
+ }
+`;
+
+export const NavigationContainer = styled.div`
+ display: flex;
+ flex-direction: row;
+ justify-content: space-evenly;
+ align-items: center;
+ position: relative;
+ color: black;
+ font-size: 1.5em;
+ font-weight: bold;
+ padding: 10px;
+ border-radius: 25px;
+ box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+ background: gray;
+ margin: 30px 30px;
+`;
+
+export const NavigationDivContainer = styled.div`
+ display: flex;
+ flex-direction: row;
+ justify-content: center;
+ align-items: center;
+ color: black;
+ font-size: 1.5em;
+ transition: all 0.2s ease-in-out;
+ cursor: pointer;
+
+ &:hover {
+  background: #8a8a8a;
+  color: red;
  }
 `;
