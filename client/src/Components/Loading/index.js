1e22ad378969ecf189a90c32c02dc449432f57ce
Nathan Innocent Amy <89917308+OnePunchKo@users.noreply.github.com>
2022-04-18 16:03:45 -0400

    Fixed loading animation

--- client/src/Components/Loading/index.js
+++ client/src/Components/Loading/index.js
@@ -3,8 +3,8 @@ import { GIFS } from "../../Helpers/Icons";
 export const Loading = () => {
  return (
   <>
-   <div style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center" }}>
-    <div>loading...</div>
+   <div style={{ display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column", marginTop: "200px" }}>
+    <h2>loading...</h2>
     <img src={GIFS.togepi} alt="loading" />
    </div>
   </>
